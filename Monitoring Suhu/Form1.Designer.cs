0752c811b98a69d90d1dcc325ea29e834d7a0661
Muhammad Andyk Maulana (Brilliant Ace) <muhammadandykmaulana@gmail.com>
2018-05-17 13:37:46 +0700

    Initial Commit

---
+++ Monitoring Suhu/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Monitoring_Suhu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.detak = new System.Windows.Forms.Timer(this.components);
+            this.zedGraphSuhu = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(12, 263);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 1;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(121, 263);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 2;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // detak
+            // 
+            this.detak.Tick += new System.EventHandler(this.detak_Tick);
+            // 
+            // zedGraphSuhu
+            // 
+            this.zedGraphSuhu.Location = new System.Drawing.Point(12, 35);
+            this.zedGraphSuhu.Name = "zedGraphSuhu";
+            this.zedGraphSuhu.ScrollGrace = 0D;
+            this.zedGraphSuhu.ScrollMaxX = 0D;
+            this.zedGraphSuhu.ScrollMaxY = 0D;
+            this.zedGraphSuhu.ScrollMaxY2 = 0D;
+            this.zedGraphSuhu.ScrollMinX = 0D;
+            this.zedGraphSuhu.ScrollMinY = 0D;
+            this.zedGraphSuhu.ScrollMinY2 = 0D;
+            this.zedGraphSuhu.Size = new System.Drawing.Size(268, 222);
+            this.zedGraphSuhu.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 349);
+            this.Controls.Add(this.zedGraphSuhu);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Timer detak;
+        private ZedGraph.ZedGraphControl zedGraphSuhu;
+    }
+}
+
---
+++ Monitoring Suhu/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//namespace untuk zedGrpah
+using ZedGraph;
+//namespace untuk class serial
+using System.IO.Ports;
+//namespace untuk melihat setting global komputer
+using System.Globalization;
+//using ZedGraph.PointPairList;
+namespace Monitoring_Suhu
+{
+    public partial class Form1 : Form
+    {
+        SerialPort arduino = new SerialPort("COM4", 9600);
+        double waktuStart = 100;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+             try
+            {
+                btn_start.Enabled = false;
+                btn_stop.Enabled = true;
+                arduino.ReadTimeout = 1000;
+                arduino.WriteTimeout = 1000;
+                detak.Enabled = true;
+                //mulai detak
+                detak.Start();
+            }
+            catch (Exception gagal)
+            {
+                //error handling
+                //MessageBox.Show(gagal.ToString());
+            }
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //nyalakan kembali tombol start
+                btn_start.Enabled = true;
+                //matikan tombol stop
+                btn_stop.Enabled = false;
+                //stop detak
+                detak.Stop();
+                //matikan detak
+                detak.Enabled = false;
+                //jika komunikasi masih terbuka, tutup komunikasi
+                if (arduino.IsOpen)
+                {
+                    arduino.Close();
+                }
+            }
+            catch (Exception gagal)
+            {
+                //error handling
+                //MessageBox.Show(gagal.ToString());
+            }
+        
+        }
+
+        private void detak_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                //Mulai komunikasi dengan arduino
+                arduino.Open();
+
+                //get item kurva pertama pada grafik
+                LineItem kurvaSuhu = zedGraphSuhu.GraphPane.CurveList[0] as LineItem;
+
+                //get PointPairList
+                IPointListEdit listSuhu = kurvaSuhu.Points as IPointListEdit;
+
+                //waktu yang terlewati
+                double waktu = (Environment.TickCount - waktuStart) / 1000.0;
+
+                //baca data suhu yang dikirim arduino,
+                //konversi data tersebut (string) ke double,
+                //dan tambahkan ke listSuhu
+                float dataSuhu = float.Parse(arduino.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
+                listSuhu.Add(waktu, Convert.ToDouble(dataSuhu));
+
+                //Tutup komunikasi dengan arduino
+                arduino.Close();
+
+                //Buat scale X tetap rolling dalam interval 30 detik, dengan satu
+                //langkah besar antara nilai X maksimal dan akhir dari axis
+                Scale xScale = zedGraphSuhu.GraphPane.XAxis.Scale;
+                if (waktu > xScale.Max - xScale.MajorStep)
+                {
+                    xScale.Max = waktu + xScale.MajorStep;
+                    xScale.Min = xScale.Max - 30.0;
+                }
+
+                // Pastikan Y axis di scale ulang untuk mengakomodir data aktual
+                zedGraphSuhu.AxisChange();
+
+                // Redraw paksa
+                zedGraphSuhu.Invalidate();
+
+            }
+            catch (Exception gagal)
+            {
+                //error handling
+                //tutup komunikasi dengan arduino jika masih terhubung
+                if (arduino.IsOpen)
+                {
+                    arduino.Close();
+                }
+                //MessageBox.Show(gagal.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //catat waktu pertama sebagai referensi
+                waktuStart = Environment.TickCount;
+
+                //Inisialisasi ZedGraph
+                GraphPane grafikSuhu = zedGraphSuhu.GraphPane;
+                grafikSuhu.Title.Text = "Grafik Suhu";
+                grafikSuhu.XAxis.Title.Text = "Waktu (Detik)";
+                grafikSuhu.YAxis.Title.Text = "Suhu (Celcius)";
+
+                //Simpan 120 point. Pada 500 ms sampel rate.
+                //RollingPointPairList adalah class penyimpanan yang efisien 
+                //dengan tetap merolling kumpulan point data tanpa perlu
+                //men-shift nilai data apapun
+                RollingPointPairList listSuhu = new RollingPointPairList(120);
+
+                //Inisialisasi kurva
+                LineItem kurvaSuhu = grafikSuhu.AddCurve("Suhu", listSuhu, Color.Red, SymbolType.None);
+                
+            }
+            catch (Exception gagal)
+            {
+                //error handling
+                //MessageBox.Show(gagal.ToString());
+            }
+        }
+        
+    }
+}
